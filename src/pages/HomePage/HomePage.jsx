4fc8b37ac93e3bc0d2ec50b6510ad6a7caba1ec7
“GabrielVScalese” <gabriel.scalese@hotmail.com>
2024-11-14 23:11:26 -0300

    feat: validation and transition

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
-import { Box, Button, Icon, TextField, Typography } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { Box, Button, TextField, Typography, Zoom } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const HomePage = () => {
-  const [tasks, setTasks] = useState([
-    "Tarefa 1",
-    "Tarefa 2",
-    "Tarefa 3",
-    "Tarefa 4",
-  ]);
-
+  const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState("");
 
-  const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask("");
-  };
+  const addTask = useCallback(() => {
+    if (tasks.find((value) => value == task)) {
+      alert("Tarefa já existente!");
+      return;
+    }
 
-  const deleteTask = (taskToDelete) => () =>
-    setTasks(tasks.filter((savedTask) => savedTask != taskToDelete));
+    if (task !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  }, [task]);
+
+  const deleteTask = useCallback(
+    (taskToDelete) => () =>
+      setTasks(tasks.filter((savedTask) => savedTask != taskToDelete)),
+    [tasks]
+  );
 
   return (
     <Box
@@ -30,51 +34,57 @@ const HomePage = () => {
       alignItems="center"
       gap="20px"
     >
-      <Typography fontSize={30} textAlign="center">
-        Minhas Tarefas
-      </Typography>
-      <Box
-        height="800px"
-        width="800px"
-        border="1px solid #d1d1e0"
-        borderRadius="5px"
-        padding="20px"
-        display="flex"
-        flexDirection="column"
-        gap="20px"
-      >
-        <Box width="100%" display="flex" flexDirection="row" gap="10px">
-          <TextField
-            variant="outlined"
-            label="Nome da tarefa"
-            width="300px"
-            fullWidth
-            value={task}
-            onChange={(event) => setTask(event.target.value)}
-          ></TextField>
-          <Button variant="contained" onClick={addTask}>
-            Adicionar
-          </Button>
+      <Zoom in={true}>
+        <Typography fontSize={30} textAlign="center">
+          Minhas Tarefas
+        </Typography>
+      </Zoom>
+      <Zoom in={true}>
+        <Box
+          height="70%"
+          width="30%"
+          border="1px solid #d1d1e0"
+          borderRadius="5px"
+          padding="20px"
+          display="flex"
+          flexDirection="column"
+          gap="20px"
+        >
+          <Box width="100%" display="flex" flexDirection="row" gap="10px">
+            <TextField
+              variant="outlined"
+              label="Nome da tarefa"
+              width="300px"
+              fullWidth
+              value={task}
+              onChange={(event) => setTask(event.target.value)}
+            ></TextField>
+            <Button variant="contained" onClick={addTask}>
+              Adicionar
+            </Button>
+          </Box>
+          <Box display="flex" gap="15px" flexDirection="column" overflow="auto">
+            {tasks.map((savedTask) => (
+              <Zoom in={true}>
+                <Box
+                  border="1px solid #d1d1e0"
+                  borderRadius="5px"
+                  padding="12px"
+                  display="flex"
+                  flexDirection="row"
+                  justifyContent="space-between"
+                >
+                  <Typography>{savedTask}</Typography>
+                  <DeleteIcon
+                    sx={{ color: "red", cursor: "pointer" }}
+                    onClick={deleteTask(savedTask)}
+                  />
+                </Box>
+              </Zoom>
+            ))}
+          </Box>
         </Box>
-        <Box display="flex" gap="15px" flexDirection="column">
-          {tasks.map((savedTask) => (
-            <Box
-              border="1px solid #d1d1e0"
-              borderRadius="5px"
-              padding="12px"
-              display="flex"
-              flexDirection="row"
-              justifyContent="space-between"
-            >
-              <Typography>{savedTask}</Typography>
-              <DeleteIcon
-                sx={{ color: "red", cursor: "pointer" }}
-                onClick={deleteTask(savedTask)}
-              />
-            </Box>
-          ))}
-        </Box>
-      </Box>
+      </Zoom>
     </Box>
   );
 };
